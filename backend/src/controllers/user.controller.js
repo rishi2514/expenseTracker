483cc032533ca7b460642e3d48eddbe8bfcffca6
rishi2514 <rsy81179105@gmail.com>
2026-09-15 11:30:35 +0530

    feat: implement logoutUser function to handle user logout and clear tokens

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -151,7 +151,26 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  
-})
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: { refreshToken: undefined },
+    },
+    {
+      new: true,
+    }
+  );
 
-export { registerUser, loginUser };
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie(accessToken, options)
+    .clearCookie(resfreshToken, options)
+    .json(new ApiResponse(200, {}, "User logged out successfully."));
+});
+
+export { registerUser, loginUser, logoutUser };
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,14 +1,22 @@
-import { Router } from "express"
-import {registerUser} from "../controllers/user.controller.js"
-import {upload} from "../middlewares/multer.middleware.js"
+import { Router } from "express";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 // creating router from express.router
-const router = Router()
+const router = Router();
 
 // for using the middleware just before calling our controller we add another function in this case we called upload function from utils for processing image and then proceed to registerUser
-router.route("/register").post(
-    upload.single("avatar"),
-    registerUser
-)
+router.route("/register").post(upload.single("avatar"), registerUser);
 
-export default router
+router.route("/login").post(loginUser);
+
+// secured routes
+
+router.route("/logout").post(verifyJWT, logoutUser);
+
+export default router;
